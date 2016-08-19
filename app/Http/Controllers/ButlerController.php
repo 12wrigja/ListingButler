c889fcb8daa8745f38b0e7dbf0078320734c6372
vagrant <vagrant@homestead>
2016-08-19 00:49:19 +0000

    Method calling from /butler command. WIP.
    Signed-off-by: vagrant <vagrant@homestead>

--- app/Http/Controllers/ButlerController.php
+++ app/Http/Controllers/ButlerController.php
@@ -9,22 +9,38 @@ use App\Http\Requests;
 class ButlerController extends Controller
 {
     public function index(){
-	$response = '';
+	/*$response = '';
 	foreach(request()->all() as $key=>$value){
 		$response = $response . $key . '\t' . $value . '\n';
 	}
 	return $response;
+	*/
 	$token = request()->input('token');
 	if($token == null){
 		return response('Invalid Token',400);
 	}
 
 	$configToken = env('SLACK_TOKEN');
-	dd($configToken);
-	if(config('SLACK_TOKEN') == request()->get('token')){
-		return 'OK';
-	} else {
-		return 'Not OK';
+	if($configToken != $token){
+		return response('Invalid Token',400);
 	}
+
+	$text = request()->input('text');
+
+	$argsSplit = preg_split('#\s+#',$text,null,PREG_SPLIT_NO_EMPTY);
+
+	if(count($argsSplit) == 0){
+		return $this->help();
+	}
+	
+	$method = $argSplit[0];
+	if(method_exists($this,$method)){
+		if(count($argSplit) == 1){
+			return $this->$method();
+		} else {
+			//TODO call methods with arguments here.
+		}
+	}
+	
 	}
 }
